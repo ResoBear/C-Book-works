6bff0e9319d301a90686bbebf6f42940d414e338
unknown <resobear@gmail.com>
2018-12-13 19:04:16 +0100

    Update13.12

---
+++ Ćwiczenia/14_1/LiczbaCalkowita.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_1
+{
+    class LiczbaCalkowita
+    {
+        int x;
+
+
+        int Conv()
+        {
+            int y = x * x;
+            return y;
+        }
+
+        public int Podaj()
+        {
+            Console.WriteLine("Podaj x:");
+            x = Convert.ToInt32(Console.ReadLine());
+            x = Conv();
+            return x;
+        }
+
+        public void Wyswietl()
+        {
+            Console.WriteLine("Wynik: {0}", x);
+            Pisz();
+        }
+        void Pisz()
+        {
+
+            Console.WriteLine(Conv());
+        }
+    }
+}
---
+++ Ćwiczenia/14_2/Prostokat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_2
+{
+    class Prostokat
+    {
+        public int a, b, c, d;
+
+
+    }
+}
