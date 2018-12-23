94f5f51c2badb5fe7ea30a87b3904e4e60e0d0e4
unknown <resobear@gmail.com>
2018-12-23 18:26:40 +0100

    Update 16_5

---
+++ Ćwiczenia/16_5/Kwadrat.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_5
+{
+    class Kwadrat
+    {
+        int x,y;
+        int wys, szer;
+
+        public Kwadrat(int LewyGornyX, int LewyGornyY, int wysokosc, int szerokosc) // pobieranie danych
+        {
+            x = LewyGornyX;
+            y = LewyGornyY;
+
+            wys = wysokosc;
+            szer = szerokosc;
+
+        }
+
+        int LewyY(int y, int wys) //Obliczenie Lewego Y
+        {
+            int Ly = y - wys;
+            return Ly;
+        }
+
+        int PrawyX(int x, int szer) //Obliczenie Prawego X
+        {
+            int Px = x + szer;
+            return Px;
+        }
+
+        void PrawyDolny() // Prawy dolny bok
+        {
+
+            Console.WriteLine("Prawy Dolny: \nx = {0}\ny = {1}\n", PrawyX(x , szer) ,LewyY(y,wys));
+        }
+
+        void LewyDolny() // Lewy dolny bok
+        {
+
+            Console.WriteLine("Lewy Dolny: \nx = {0}\ny = {1}\n", x, LewyY(y,wys));
+        }
+
+        void PrawyGórny() // Prawy Górny bok
+        {
+
+            Console.WriteLine("Prawy Górny: \nx = {0}\ny = {1}\n", PrawyX(x, szer), y);
+        }
+
+        void LewyGorny()
+        {
+            Console.WriteLine("Lewy Górny: \nx = {0}\ny = {1}\n", x, y);
+        }
+        public void Wypisz() // Wypisanie wszystkiego
+        {
+            LewyGorny();
+            LewyDolny();
+            PrawyGórny();
+            PrawyDolny();
+
+
+        }
+    }
+}
---
+++ Ćwiczenia/16_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x, y, wys, szer;
+            Console.WriteLine("Program podaje współżędne kwadratu po współżędnych Lewego górnego boku," +
+                "Wysokości i szerokości");
+
+            Console.WriteLine("Podaj x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj Wysokosc: ");
+            wys = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj szerokosc: ");
+            szer = Convert.ToInt32(Console.ReadLine());
+
+            Kwadrat LewyGorny = new Kwadrat(x, y, wys, szer);
+
+            LewyGorny.Wypisz();
+
+        }
+    }
+}
